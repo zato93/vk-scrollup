de8f30eb8e749abc9e476869db38c77202d78011
zato <zato93one@gmail.com>
2016-02-12 22:14:51 +0600

    Signed-off-by: zato <zato93one@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# vk-scrollup
+# My first repo - vk-scrollup
+There is scroll up button from vk.com
+
+Just put this to your html:
+
+```html
+<script src="path/vk.scrollup.zato93.js" type="text/javascript"></script>
+```
+
+regards,
+Arman
---
+++ vk.scrollup.zato93.js
@@ -0,0 +1,48 @@
+(function() {
+	var up='Наверх';
+	$('body').append('<div class="go-up" style="display:none"><div class="up"><span class="glyphicon glyphicon-triangle-top"></span> <b>'+up+'</b></div><div class="down" style="display:none"><span class="glyphicon glyphicon-triangle-bottom"></span></div><style>.go-up{position: fixed;width: 120px;background-color: white;opacity: 0.2;left: 0;top:0;height: 100%;z-index: -1;cursor: pointer;padding: 10px 15px;}.go-up:hover{opacity: 0.5;}</style></div>');
+	var $el = $('.go-up'),
+		
+		lastTop = 0,
+		status = 0,
+		w = $(window);
+	w.on('scroll', function(data) {
+		if (w.scrollTop() > 0) {
+			scrollTrigger(1);
+		} else {
+			if (lastTop) {
+				scrollTrigger(2);
+			} else {
+				scrollTrigger(0);
+			}
+		}
+	});
+	$el.on('click', function(data) {
+		if (status == 1) {
+			lastTop = w.scrollTop();
+			w.scrollTop(200);
+			$('body').animate({
+				scrollTop: 0
+			}, 100);
+		} else {
+			w.scrollTop(lastTop);
+			scrollTrigger(1);
+		}
+
+	});
+	var scrollTrigger = function(a) {
+		if (!a) {
+			$el.hide();
+		} else {
+			$el.show();
+			if (a == 1) {
+				$('.go-up .up').show();
+				$('.go-up .down').hide();
+			} else if (a == 2) {
+				$('.go-up .up').hide();
+				$('.go-up .down').show();
+			}
+		}
+		status = a;
+	};
+})();
